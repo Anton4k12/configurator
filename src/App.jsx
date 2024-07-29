c3ea21d582ce99705606e72aa6d63d1d53a39b36
Anton4k12 <neanton2008@gmail.com>
2024-07-29 20:08:42 +0300

    setting up new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-function App() {
-  const [count, setCount] = useState(0)
+import useSWR from "swr";
+import { Button } from "./components/ui/button";
 
+function Profile() {
+  const { data, error, isLoading } = useSWR(
+    "https://api.github.com/users/Anton4k12",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log(data);
+
+  // render data
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="border rounded-md flex p-4 w-fit gap-4 items-center shadow">
+      <img className="size-20 rounded-full" src={data.avatar_url}></img>
+      <div className="flex flex-col">
+        <div className="font-bold text-xl">{data.name}</div>
+        <Button asChild size="xs">
+          <a href={data.html_url} target="_blank">
+            Go to profile
+          </a>
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default Profile;
