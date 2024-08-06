86feaa9b5a649cfb0c3b7ab6e636de10c3ef0c0c
Anton4k12 <neanton2008@gmail.com>
2024-08-06 22:22:27 +0300

    grid skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export function App() {
       <div className="max-w-screen-xl border mx-auto min-h-screen">
         <div
           aria-label="main text"
-          className="flex flex-col gap-10 w-1/2 pt-44 pl-20"
+          className="flex flex-col gap-10 w-1/2 pt-44 pl-20 pb-[72px]"
         >
           <h1 className="text-6xl font-extralight">Build your own</h1>
           <div className="flex flex-col items-start gap-7">
@@ -26,9 +26,14 @@ export function App() {
 
         <hr className="border-zinc-600" />
 
-        <div aria-label="cars selector">
-          <Car name="GranTurismo" imageUrl="/home/cars/GranTurismo.webp"></Car>
-          <Car name="GranCabrio" imageUrl="/home/cars/GranCabrio.webp"></Car>
+        <div aria-label="cars selector" className="grid w-full">
+          <div className="flex">
+            <Car
+              name="GranTurismo"
+              imageUrl="/home/cars/GranTurismo.webp"
+            ></Car>
+            <Car name="GranCabrio" imageUrl="/home/cars/GranCabrio.webp"></Car>
+          </div>
         </div>
       </div>
     </>
--- src/components/home/car.jsx
+++ src/components/home/car.jsx
@@ -1,8 +1,14 @@
+import { ChevronRight } from "../icons/chevron-right";
+
 export const Car = ({ name, imageUrl }) => {
   return (
-    <div>
-      <div>{name}</div>
-      <img src={imageUrl} />
+    <div className="flex flex-col items-center gap-4">
+      <div className="text-2xl pt-10 ">{name}</div>
+      <img src={imageUrl} className="w-[432px] h-[215px]" />
+      <button className="uppercase border border-black px-5 py-4 relative pr-44 text-[11px] font-medium">
+        Select{" "}
+        <ChevronRight className="absolute size-4 top-1/2 -translate-y-1/2 right-3.5"></ChevronRight>
+      </button>
     </div>
   );
 };
