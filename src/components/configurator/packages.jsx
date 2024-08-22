6832d99787cb02c04e53c73486d6f769c887868f
Anton4k12 <neanton2008@gmail.com>
2024-08-22 21:28:22 +0300

    hiding packages

--- src/components/configurator/packages.jsx
+++ src/components/configurator/packages.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import { PlusIcon } from "../icons/plus-icon";
 import { Package } from "./package";
 
 export const Packages = ({ packages }) => {
-  console.log(packages);
+  const [isExtended, setIsExtended] = useState(false);
+
+  const croppedPackages = packages.slice(0, 4);
+
+  const handleExtend = () => {
+    setIsExtended(!isExtended);
+  };
+
   return (
     <div className="px-3 pt-6">
       <h2>
@@ -9,17 +18,58 @@ export const Packages = ({ packages }) => {
           Packages
         </span>
       </h2>
-      <div className="grid grid-cols-2 gap-6 pt-5">
-        {packages.map((pack) => {
-          return (
-            <Package
-              name={pack.name}
-              imageUrl={pack.imageUrl}
-              price={pack.price}
-              characteristics={pack.characteristics}
-            ></Package>
-          );
-        })}
+
+      {!isExtended ? (
+        <div className="grid grid-cols-2 gap-6 pt-5">
+          {croppedPackages.map((pack) => {
+            return (
+              <Package
+                name={pack.name}
+                imageUrl={pack.imageUrl}
+                price={pack.price}
+                characteristics={pack.characteristics}
+              ></Package>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 gap-6 pt-5">
+          {packages.map((pack) => {
+            return (
+              <Package
+                name={pack.name}
+                imageUrl={pack.imageUrl}
+                price={pack.price}
+                characteristics={pack.characteristics}
+              ></Package>
+            );
+          })}
+        </div>
+      )}
+
+      <div className="flex flex-col items-center pt-5">
+        {!isExtended ? (
+          <div className="text-xs uppercase text-[#666]">
+            Show more packages
+          </div>
+        ) : (
+          <div className="text-xs uppercase text-[#666]">Hide packages</div>
+        )}
+
+        <div className="pt-[10px]">
+          <button
+            onClick={handleExtend}
+            className="w-fit rounded-full border border-black p-3"
+          >
+            <PlusIcon className=""></PlusIcon>
+          </button>
+        </div>
+
+        <div className="px-3 pt-[30px] text-center font-light tracking-[0.7px]">
+          * Note: package choices may require other mandatory packages or
+          options, depending on country. The price of each package may change
+          according to the addition or deletion of other packages or options.
+        </div>
       </div>
     </div>
   );
