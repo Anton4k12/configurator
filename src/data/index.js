eb7b54928ccbb627a269ec7c3b2ee7abf3345d52
Anton4k12 <neanton2008@gmail.com>
2024-09-27 20:17:32 +0300

    feat: check .env.local

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,7 @@
+if (!import.meta.env.VITE_BACKEND_URL) {
+  throw new Error("No VITE_BACKEND_URL in .env.local");
+}
+
 export const fetcher = (url, ...args) =>
   fetch(import.meta.env.VITE_BACKEND_URL + url, ...args).then((res) =>
     res.json(),
